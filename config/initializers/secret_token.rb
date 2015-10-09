d06274fe4c6e3e8e868d64b2d9bfddecae5386f3
Neha Jain <njain@linkedin.com>
2015-10-09 14:12:38 -0700

    First commit to get started with ember

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureYourself::Application.config.secret_token = '54114f8d62fd082d4b8951806a252f8576bc45417360ea29108276f4d1528e7ce4f54260d9a9f0c0f517737370284fa8ea9f067f135b79a8fa88477a35def0ea'
